86cb9f02ebc5096be69160dd470028342b145d5a
YumYumae <118685311+YumYumae@users.noreply.github.com>
2026-05-06 14:23:09 +0200

    Commit - First part

--- src/exo/Partie1.java
+++ src/exo/Partie1.java
@@ -2,22 +2,34 @@ package exo;
 
 import models.Trip;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class Partie1 {
 
     public List<Trip> longAndExpensiveTrips(List<Trip> trips) {
         // distance > 10km et prix > 20€
-        return List.of();
+        return trips.stream()
+            .filter(trip -> trip.distanceKm() > 10 && trip.price() > 20)
+            .toList();
     }
 
     public List<Trip> badTrips(List<Trip> trips) {
         // rating < 3
-        return List.of();
+        return trips.stream()
+            .filter(trip -> trip.rating() < 3)
+            .toList();
     }
 
     public List<Trip> recentTrips(List<Trip> trips) {
         // aujourd’hui ou hier
-        return List.of();
+        LocalDate today = LocalDate.now();
+        LocalDate yesterday = today.minusDays(1);
+        return trips.stream()
+            .filter(trip -> {
+                LocalDate tripDate = trip.startTime().toLocalDate();
+                return tripDate.equals(today) || tripDate.equals(yesterday);
+            })
+            .toList();
     }
 }
